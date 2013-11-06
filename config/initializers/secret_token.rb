ccbfcfb5862c09619b42340d272c44973823f3c1
David Padilla <david@crowdint.com>
2013-11-05 22:54:18 -0600

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogMagmaconfCom::Application.config.secret_key_base = '6ddd472472f3778043ea7088b3c37562c3a0d3aa7cbf1c7f68d32fac78f452da80f9472445dcb99a3a4ea7e3958dbf7aa3f3e9ba649b2c9515d8d1d28ddec2e7'
