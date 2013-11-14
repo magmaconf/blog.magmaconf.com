22a4184a83b8b2fa2770b12377e3d6c85598288d
Giovanni Mendoza <mendozagioo@gmail.com>
2013-11-13 18:03:35 -0600

    Previous and Next from i18n changed in translations

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,5 +1,3 @@
 WillPaginate.per_page = 5
 WillPaginate::ViewHelpers.pagination_options[:page_links] = false
 WillPaginate::ViewHelpers.pagination_options[:container] = false
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = 'Prev'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Next'
