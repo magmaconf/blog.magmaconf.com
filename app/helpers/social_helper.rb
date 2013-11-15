cc841a63578f04e5dbc167f3eaa0462b3e49cf37
Giovanni Mendoza <mendozagioo@gmail.com>
2013-11-15 11:21:40 -0600

    Helper modified to add icons inside of each share-social-link

--- app/helpers/social_helper.rb
+++ app/helpers/social_helper.rb
@@ -1,17 +1,23 @@
 module SocialHelper
   def facebook_share(post)
     url = "http://www.facebook.com/sharer/sharer.php?s=100&p[url]=#{social_share_url(post)}&p[images][0]=&p[title]=#{social_share_title(post)}&p[summary]="
-    link_to 'facebook', url, target: "_BLANK"
+    link_to url, target: "_BLANK" do
+      content_tag(:i, '', class: 'fa fa-facebook')
+    end
   end
 
   def twitter_share(post)
     url = "http://twitter.com/home?status=#{social_share_title(post)} #{social_share_url(post)} via @crowdint"
-    link_to 'twitter', url, target: "_BLANK"
+    link_to url, target: "_BLANK" do
+      content_tag(:i, '', class: 'fa fa-twitter')
+    end
   end
 
   def gplus_share(post)
     url = "https://plus.google.com/share?url=#{social_share_url(post)}"
-    link_to 'gplus', url, target: "_BLANK"
+    link_to url, target: "_BLANK" do
+      content_tag(:i, '', class: 'fa fa-google-plus')
+    end
   end
 
   def social_share_url(post)
